964fac8496716d750fa90d4eeeeaa294e3b14409
YugCh <yugchauhan2000@gmail.com>
2017-01-15 11:31:16 -0500

    Second commit

---
+++ app/src/main/java/com/example/yug/huronhighschoolapp/BellSchedule.java
@@ -0,0 +1,13 @@
+package com.example.yug.huronhighschoolapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BellSchedule extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bell_schedule);
+    }
+}
---
+++ app/src/main/java/com/example/yug/huronhighschoolapp/Calendar.java
@@ -0,0 +1,13 @@
+package com.example.yug.huronhighschoolapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Calendar extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
