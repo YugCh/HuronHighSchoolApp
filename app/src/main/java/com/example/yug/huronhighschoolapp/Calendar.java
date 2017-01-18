d30e69d627ec06a828371762f8cbc6111424d089
YugCh <yugchauhan2000@gmail.com>
2017-01-18 18:37:03 -0500

    Fourth commit, need to find a Calendar link

--- app/src/main/java/com/example/yug/huronhighschoolapp/Calendar.java
+++ app/src/main/java/com/example/yug/huronhighschoolapp/Calendar.java
@@ -15,6 +15,6 @@ public class Calendar extends Activity {
         WebView viewCalendar = (WebView) findViewById(R.id.CalendarView);
         WebSettings webSettings = viewCalendar.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        viewCalendar.loadUrl("https://calendar.google.com/calendar/embed?src=aaps.k12.mi.us_bkfpnid9pe5438jl75bschj5k4%40group.calendar.google.com&ctz=America/New_York");
+        viewCalendar.loadUrl("https://www.google.com/calendar/embed?title=Huron%20Calendars&amp;showTitle=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=17p3uh9vm0nh41tgq1hnr27u08%40group.calendar.google.com&amp;color=%23528800&amp;src=ner10ng8t3u9mun9tbp2tivcdk%40group.calendar.google.com&amp;color=%23125A12&amp;src=dnu292q8k6gqlbmf32dcgvoor4n5mp6n%40import.calendar.google.com&amp;color=%23856508&amp;src=ujqpuakfhi0eln5pipt4je0o2hjd6mgp%40import.calendar.google.com&amp;color=%23856508&amp;src=fj1m75p09gbpreeru9nta0eevc%40group.calendar.google.com&amp;color=%234E5D6C&amp;ctz=America%2FNew_York");
     }
 }
--- app/src/main/java/com/example/yug/huronhighschoolapp/MainActivity.java
+++ app/src/main/java/com/example/yug/huronhighschoolapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yug.huronhighschoolapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -68,8 +69,8 @@ public class MainActivity extends Activity {
         {
             public void onClick(View v)
             {
-                Uri PSchool = Uri.parse("http://pschool.aaps.k12.mi.us");
-                Intent PSchool = new Intent(Intent.ACTION_VIEW, PSchool);
+                Uri PSchoolURL = Uri.parse("http://pschool.aaps.k12.mi.us");
+                Intent PSchool = new Intent(Intent.ACTION_VIEW, PSchoolURL);
                 startActivity(PSchool);
             }
         });
